570fba812a1c027b213417a9651d563eb59b379b
Gabriel Carvalho <gabriel.cvsilva1@gmail.com>
2024-02-22 08:32:37 -0300

    contando os votos e criando um websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,28 @@
 import fastify from 'fastify';
 import cookie from '@fastify/cookie';
+import websocket from '@fastify/websocket';
+
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-polls';
 import { voteOnPoll } from './routes/vote-on-poll';
+import { pollsResults } from './ws/polls-result';
 
 const app = fastify();
 
+// Cookies
 app.register(cookie, {
     secret: "polls-app-nlw",
     hook: "onRequest"
 });
 
+// Websockets
+app.register(websocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollsResults);
 
 app.listen({port: 3333}).then(() => {
     console.log('Servidor Iniciado')
